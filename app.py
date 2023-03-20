97e7ac20eb692390851923c843da3c49a834168e
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-20 12:29:11 -0700

    on part two

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+import os
+from flask import Flask, jsonify, request
+from models import Cupcake, db, connect_db
 """Flask app for Cupcakes"""
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secret"
+
+connect_db(app)
+
+@app.get('/')
+def display_cupcakes():
+    """ Lists all cupcakes """
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+
+    return jsonify(cupcakes=serialized)
+
--- models.py
+++ models.py
@@ -1,1 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMG = "https://tinyurl.com/demo-cupcake"
+
 """Models for Cupcake app."""
+class Cupcake(db.Model):
+    """ Cupcake """
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    flavor = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    size = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    rating = db.Column(
+        db.Integer(),
+        nullable=False,
+    )
+
+    image = db.Column(
+        db.Text,
+        nullable=False,
+        default=DEFAULT_IMG
+    )
+
+    def serialize(self):
+        """ Serialize to dict """
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
